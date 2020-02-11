2e9652bce8f5ddba0a090b17927591790e90f518
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2020-02-11 16:39:50 -0500

    Add project BibliotecaLibros

---
+++ BibliotecaLibros/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BibliotecaLibros.MvcApplication" Language="C#" %>
---
+++ BibliotecaLibros/Models/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaLibros.Models
+{
+    public class Libro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Descripción { get; set; }
+        public string Imagen { get; set; }
+        public int Stock { get; set; }
+    }
+}
