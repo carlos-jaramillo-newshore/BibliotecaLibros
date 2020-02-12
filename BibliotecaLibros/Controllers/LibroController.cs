9ff50b6c2edef7c259c5554c71edc981263bc6c9
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2020-02-12 05:19:16 -0500

    Add model, controller and view

---
+++ BibliotecaLibros/Controllers/LibroController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BibliotecaLibros.Models;
+
+namespace BibliotecaLibros.Controllers
+{
+    public class LibroController : Controller
+    {
+        private BibliotecaLibrosEntities db = new BibliotecaLibrosEntities();
+
+        // GET: Libro
+        public ActionResult Index()
+        {
+            return View(db.Libro.ToList());
+        }
+
+        [HttpPost]
+        public string Prestamo(int IdLibro)
+        {
+            var libro = db.Libro.Find(IdLibro);
+
+            if (libro.Stock > 0)
+            {
+                libro.Stock -= 1;
+                db.SaveChanges();
+            }
+            else
+            {
+                libro.Stock = -1;
+            }
+
+            return Newtonsoft.Json.JsonConvert.SerializeObject(libro);
+        }
+
+        // GET: Libro/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libro.Find(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        // GET: Libro/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Libro/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdLibro,Titulo,Autor,Descripcion,Imagen,Stock")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Libro.Add(libro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(libro);
+        }
+
+        // GET: Libro/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libro.Find(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        // POST: Libro/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdLibro,Titulo,Autor,Descripcion,Imagen,Stock")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(libro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(libro);
+        }
+
+        // GET: Libro/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libro.Find(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        // POST: Libro/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Libro libro = db.Libro.Find(id);
+            db.Libro.Remove(libro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BibliotecaLibros/Scripts/MyScript.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true,
+    });
+    $('.tooltipped').tooltip();
+});
+
+function PrestarLibro(Id) {
+    $.ajax({
+        url: '/Libro/Prestamo',
+        dataType: 'json',
+        type: 'post',
+        data: { IdLibro: Id },
+        success: function (datos) {
+            if (datos.Stock != -1) {
+                $('.stock').html(datos.Stock);
+            } else {
+                $('.stock').html(0);
+                alert('No hay más libros de este título');
+            }
+        },
+        error: function (datos) {
+            alert("Ocurrió un error");
+        }
+    });
+}
